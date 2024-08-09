5521b2f671bf05815d8c3795a1e5e542b1b1cb78
Tsmorz <tonysmoragiewicz@gmail.com>
2024-08-08 22:51:46 -0400

    add rp2040 as default device

--- src/definitions.py
+++ src/definitions.py
@@ -1,9 +1,9 @@
 import os
 
 BAUD_RATE = 57600  # bps
-COMMON_SERIAL_PORT_STRING = "usbmodem"
 SERIAL_PORT_TIMEOUT = 1.0  # seconds
 DECODING = "utf-8"
+DEFAULT_DEVICE = "RP2040"
 
 GPS_NO_FIX_STRING = "Waiting for GPS fix..."
 
@@ -12,4 +12,4 @@ FLIGHT_LOG_FILENAME = "flight-logs"
 GPS_FILE = os.path.join(DATA_DIR, "gps-data.txt")
 IMU_FILE = os.path.join(DATA_DIR, "imu-data.txt")
 
-BOSTON_LAT_LON = (42 + 19.5617980957031250 / 60, -71 - 7.4340000152587891 / 60)
+BOSTON_LAT_LON = (42.33, -71.15)
