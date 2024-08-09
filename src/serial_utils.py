0a22f1dea8589e4de918200d2631a509966f321e
Tsmorz <tonysmoragiewicz@gmail.com>
2024-08-08 21:44:23 -0400

    write serial port data to a text file

--- src/serial_utils.py
+++ src/serial_utils.py
@@ -2,12 +2,13 @@
 
 import time
 import os
-from datetime import datetime
+import argparse
 
 from loguru import logger
 import serial
 from serial.tools import list_ports
 
+from misc_utils import date_time_string
 from definitions import (
     BAUD_RATE,
     COMMON_SERIAL_PORT_STRING,
@@ -19,18 +20,21 @@ from definitions import (
 )
 
 
-current_date_time = datetime.now()
-file_ending = (
-    f"{current_date_time.year}-{current_date_time.month}-{current_date_time.day}"
+data_log_filename = os.path.join(
+    DATA_DIR, FLIGHT_LOG_FILENAME, date_time_string() + ".txt"
 )
-data_log_filename = os.path.join(DATA_DIR, FLIGHT_LOG_FILENAME, file_ending, ".txt")
 
 
-def read_serial_data(serial_port):
+def read_serial_line(serial_port):
+    """
+    Read a line of data from the serial port
+    :param serial_port:
+    :return: message from serial port
+    """
     try:
         while serial_port.in_waiting > 0:
-            word = serial_port.readline().decode(DECODING)
-            return word.strip()
+            message = serial_port.readline().decode(DECODING)
+            return message.strip()
 
     except Exception as e:
         message = f"Error reading serial data: {e}"
@@ -38,18 +42,23 @@ def read_serial_data(serial_port):
         time.sleep(0.5)
 
 
-def find_serial_port_name(string=COMMON_SERIAL_PORT_STRING):
+def find_serial_port_name(sub_string=COMMON_SERIAL_PORT_STRING):
+    """
+    Find the serial port with sub_string in the name
+    :param sub_string:
+    :return: the name of the serial port
+    """
     serial_port_name = ""
     for port in list_ports.comports():
-        if string in port.device:
+        if sub_string in port.device:
             serial_port_name = port.device
             continue
+            
     return serial_port_name
 
 
-def main():
-
-    serial_port_name = find_serial_port_name(COMMON_SERIAL_PORT_STRING)
+def stream_from_port(port_name):
+    serial_port_name = find_serial_port_name(port_name)
 
     if serial_port_name != "":
         try:
@@ -59,25 +68,42 @@ def main():
             logger.success(f"Connected to {serial_port_name}.")
             with open(data_log_filename, "w") as file:
                 while True:
-                    data = read_serial_data(serial_port)
+                    data = read_serial_line(serial_port)
                     if data is None:
-                        time.sleep(0.25)
+                        time.sleep(0.1)
                     else:
                         logger.info(data)
-                        file.write(data + "\n")
                         if data != GPS_NO_FIX_STRING:
                             file.write(data + "\n")
+
         except OSError as e:
             logger.error(e)
+
         except KeyboardInterrupt:
             if not file.closed:
                 file.close()
             logger.success("Program was ended by the user.")
 
     else:
-        logger.warning(
-            f"No ports were found with {COMMON_SERIAL_PORT_STRING} in the name."
-        )
+        logger.warning(f"No ports were found with {port_name} in the name.")
+
+    return
+
+
+def main():
+    parser = argparse.ArgumentParser("Pass in the serial port name")
+    parser.add_argument(
+        "-s",
+        "--serial-port-name",
+        required=True,
+        type=str,
+        dest="serial_port_name",
+        help="data file to simulate device",
+    )
+
+    args = parser.parse_args()
+    stream_from_port(port_name=args.serial_port_name)
+
     return
 
 
