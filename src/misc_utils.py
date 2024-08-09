7b73f0d8eedcc821bf6bdd517349c915ddedc257
Tsmorz <tonysmoragiewicz@gmail.com>
2024-08-09 01:14:27 -0400

    add doc string

--- src/misc_utils.py
+++ src/misc_utils.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
+from __future__ import annotations
 
 from datetime import datetime
 
 
-def date_time_string():
+def date_time_string() -> str:
+    """Create a string with the current date as YEARMMDD-HHMMSS :return: date
+    time string."""
     current_date_time = datetime.now()
 
     ending = (
         str(current_date_time.year)
         + str(current_date_time.month).zfill(2)
         + str(current_date_time.day).zfill(2)
-        + "-"
+        + '-'
         + str(current_date_time.hour).zfill(2)
         + str(current_date_time.minute).zfill(2)
         + str(current_date_time.second).zfill(2)
